502488f3fb269f7734953e1e7f22fa41dfd257cf
Luis Henao García <luis.henao37085@ucaldas.edu.co>
2025-04-04 14:16:56 -0500

    Obtener usuario por id, CORS

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,18 +1,42 @@
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client'); //Importamos el cliente de prisma
+const prisma = new PrismaClient(); // Creamos una instancia del cliente de Prisma
 
-const getAllUsers = async (req, res) => {
-    try {
-        const users = await prisma.users.findMany();
-        res.status(200).json(users);
-    } catch (error) {
+const getAllUsers =async(req, res)=>{
+    try{
+        const users = await prisma.users.findMany(); // Fetch all users from the database
+        res.status(200).json(users); // Send the users as a JSON response
+    }catch (error) {
+        console.log("Error fetching users:", error);
         res.status(500).json({
-        message: "Failed to fetch users",
-        error: error.message,
+            message: "Failed fetching users",
+            error: error.message
+        }); 
+    }
+
+}
+
+const getUserById=async(req,res)=>{
+    const { id }=req.params; // Extract the user ID from the request parameters
+    try{
+        const user = await prisma.users.findUnique({
+            where:{ id }
+        });
+        if(!user){
+            return res.status(404).json({
+                message:"User not found"
+            });
+        }
+        res.status(200).json(user); // Send the user as a JSON response
+    }catch(error){
+        log.error("Error fetching user:", error);
+        res.status(500).json({
+            message: "Failed fetching user",
+            error: error.message
         });
     }
-    };
+}
 
 module.exports = {
-    getAllUsers
-    };
+    getAllUsers,
+    getUserById
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ require('dotenv').config();
 const connectionDB = require('./config/database');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173" ,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
+
+    app.options("*", cors());
+
 const port= process.env.PORT || 3005;
 
 app.listen(port, ()=>{console.log(`Code running on port ${port}`)})
